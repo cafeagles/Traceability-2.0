b9e4d2e2bb892cbb6d46ef8788bbd3907fa17dc2
Justin <supermoore1025@aol.com>
2012-10-30 18:12:43 -0500

    Some of the Results part, need to add more later and testing if I can push or pull successfully.

---
+++ Traceability 2.0/src/Retriever/Results.java
@@ -0,0 +1,23 @@
+package Retriever;
+
+public class Results {
+    
+    private Link[] link;
+    
+    public int getDocCount()
+    { 
+        return 0;
+    }
+    
+    public int getKeywordFrequency(String keyword )
+    {
+        return 0;
+    }
+    
+    //public int[] getDocIds()
+    //{
+        
+    //}
+    
+    
+}
