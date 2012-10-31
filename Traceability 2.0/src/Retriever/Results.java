df58c0a1abb91817e25b9d245bba05c1fc4744f2
Justin <supermoore1025@aol.com>
2012-10-31 17:51:53 -0500

    Pushing up the newest update of results

--- Traceability 2.0/src/Retriever/Results.java
+++ Traceability 2.0/src/Retriever/Results.java
@@ -2,15 +2,34 @@ package Retriever;
 
 public class Results {
     
-    private Link[] link;
+    protected Link[] link;
+    
+    public Results()
+    {
+        
+    }
     
     public int getDocCount()
-    { 
+    {
+        //Database db = Database.getInstance();;
+        int results;
+        
+        //Query to retrieve the number of Documents
+        String query = "select count(Quantity) from Token ";
+        
+        
+        //db.submitQuery(query);
+        
+        //returns the Document count
+        
+        
+        
         return 0;
     }
     
     public int getKeywordFrequency(String keyword )
     {
+        //Database db = Database.getInstance();
         return 0;
     }
     
