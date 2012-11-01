f09a8afc277885ca8e2b928cfdcb1cf0957b2366
memphisroots <itsmesnitch@getem.ho>
2012-11-01 17:57:08 -0500

    Ima keep on pushing for Justin (results re-updated with Justins code)

--- Traceability 2.0/src/Retriever/Results.java
+++ Traceability 2.0/src/Retriever/Results.java
@@ -1,23 +1,58 @@
 package Retriever;
+import java.util.ArrayList;
 
 public class Results {
     
-    private Link[] link;
+    protected Link[] link;
+ 
+    public Results()
+    {
+        
+    }
     
     public int getDocCount()
-    { 
+    {
+        //Initialize Database
+        //Database db = Database.getInstance();
+        
+        int results;
+        
+        //Query to retrieve the number of Documents
+        String query = "select count(Quantity) from link ";
+        
+        //Submit the query
+        //db.submitQuery(query)
+        
+        
+        
+        
+        //returns the Document count
         return 0;
     }
     
     public int getKeywordFrequency(String keyword )
     {
+        //Database db = Database.getInstance();
         return 0;
     }
     
-    //public int[] getDocIds()
-    //{
-        
-    //}
     
+    public boolean greaterThan(Token t)
+    {
+        //if(this.data>t.data)
+            return true;
+    }
+    
+    /*
+Returns an array of doc Ids
+public int[] getDocIds()
+{
+//Database db = db.getInstance
+String query = "select DocId from link";
+// int[] docIds;
+//ArrayList<Integer> docIds = new ArrayList<Integer>();
+// return docIds;
+}
+*/
     
 }
